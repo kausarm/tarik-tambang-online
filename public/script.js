e6416cf94d4e902407613571ded58f14a5f30401
Muhammad Wafa <wafax.4@gmail.com>
2021-08-17 18:37:36 +0700

    mayor change: ui and engine

--- public/script.js
+++ public/script.js
@@ -1,98 +1,62 @@
-const player1 = document.getElementById("player1");
-const player2 = document.getElementById("player2");
-const mark = document.getElementById("mark");
-const rope = document.getElementById("rope");
-let interval;
-let count = 10;
+const game = document.getElementById("game")
+const player2Image = document
+  .getElementsByClassName("player2")[0]
+  .getElementsByTagName("img")[0]
+const pull = document.getElementById("tarik")
+const duration = document.getElementById("sisa-waktu")
+const player2 = document.getElementById("player2")
 
-function moveLeft() {
-    const playerLeft1 = parseInt(window.getComputedStyle(player1).getPropertyValue("left"));
-    const playerLeft2 = parseInt(window.getComputedStyle(player2).getPropertyValue("left"));
-    const ropeLeft = parseInt(window.getComputedStyle(rope).getPropertyValue("left"));
-    const markLeft = parseInt(window.getComputedStyle(mark).getPropertyValue("left"));
-    let move = Math.floor(Math.random() * 10)
-    player1.style.left = playerLeft1 - move + "px";
-    player2.style.left = playerLeft2 - move + "px";
-    rope.style.left = ropeLeft - move + "px";
-    mark.style.left = markLeft - move + "px";
-    return mark.style.left
-}
+let pos = 0
 
-function moveRight() {
-    const playerLeft1 = parseInt(window.getComputedStyle(player1).getPropertyValue("left"));
-    const playerLeft2 = parseInt(window.getComputedStyle(player2).getPropertyValue("left"));
-    const ropeLeft = parseInt(window.getComputedStyle(rope).getPropertyValue("left"));
-    const markLeft = parseInt(window.getComputedStyle(mark).getPropertyValue("left"));
-    const move = 2;
-    player1.style.left = playerLeft1 + move + "px";
-    player2.style.left = playerLeft2 + move + "px";
-    rope.style.left = ropeLeft + move + "px";
-    mark.style.left = markLeft + move + "px";
-    return mark.style.left
-}
+const COM_SPEED = 1
+const PLAYER_SPEED = 1
+let START = false
+let END = false
+let TIME = 10
+let timer
 
-const start = () => {
-    document.getElementById("start").disabled = true;
-    const timer = setInterval(() => {
-        document.getElementById("timer").innerText = count + " detik";
-        count--;
-        if (count < 0) {
-            clearInterval(timer)
-            clearInterval(interval)
-            document.getElementById("timer").innerText = "SELESAI!"
-            window.alert("SELESAI!")
+window.onload = () => {
+  player2.addEventListener("change", (e) => {
+    player2Image.setAttribute(
+      "src",
+      `https://robohash.org/${e.target.value}?set=set3`
+    )
+  })
+
+  const move = (n = -1) => {
+    if (!END) {
+      pos += n
+      game.style.transform = `translate(${pos}px)`
+    }
+  }
+
+  const start = () => {
+    if (!START && !END) {
+      START = setInterval(() => {
+        move(-COM_SPEED)
+      }, 100)
+
+      timer = setInterval(() => {
+        if (TIME <= 0) {
+          END = true
+          clearInterval(START)
+          clearInterval(timer)
+          alert(
+            pos < 0
+              ? `Yahhh.. kamu kalah`
+              : `Yeeey... selamat ${player2.value} kamu menang!`
+          )
+        } else {
+          TIME--
         }
-    }, 1000)
+        duration.innerText = TIME
+      }, 1000)
+    }
+    move(PLAYER_SPEED)
+  }
 
-    interval = setInterval(moveLeft, 400)
-
-    document.addEventListener("mousedown", ev => {
-        if (count > 0) {moveRight()}
-    })
+  pull.addEventListener("click", start)
+  document.addEventListener("keydown", (e) => {
+    if (e.key == " ") start()
+  })
 }
-
-
-// document.addEventListener("keydown", ev => {
-//     if (both === 0){
-//         both++;
-//         if (ev.key === "ArrowRight"){
-//             interval = setInterval(moveRight, 1);
-//         }
-//         if (ev.key === "ArrowLeft"){
-//             interval = setInterval(moveLeft, 1);
-//         }
-//     }
-// })
-//
-// document.addEventListener("keyup", ev => {
-//     clearInterval(interval);
-//     both = 0;
-// })
-
-
-
-
-
-
-
-// const character = document.getElementById("character");
-// const block = document.getElementById("block");
-//
-// function jump() {
-//     if (character.classList != "animate") {
-//         character.classList.add("animate");
-//     }
-//     setTimeout(function () {
-//         character.classList.remove("animate")
-//     }, 500)
-// }
-//
-// const checkDead = setInterval(function () {
-//     const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"))
-//     const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"))
-//     if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130){
-//         block.style.animation = "none";
-//         block.style.display = "none";
-//         alert("u lose");
-//     }
-// }, 10);
